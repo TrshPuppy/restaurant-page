4e1dd805850d43cd47abb318b587d63ff5a8eaa5
Rose Pineau <rosepineau@protonmail.com>
2022-09-08 17:39:59 -0400

    Add buttons in nav for tabs.

--- src/loadpage.js
+++ src/loadpage.js
@@ -5,16 +5,30 @@ export default function loadHTML() {
   const header = document.createElement("header");
   const nav = document.createElement("nav");
 
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("home-button");
+  homeButton.innerText = "Home";
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("menu-button");
+  menuButton.innerText = "Menu";
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("contact-button");
+  contactButton.innerText = "Contact";
+
+  nav.append(homeButton, menuButton, contactButton);
+
   HTMLbody.insertAdjacentElement("afterbegin", header);
   header.append(nav);
 
-  const headline = document.createElement("div");
-  headline.classList.add("headline");
-  headline.textContent = "Best Restoraunt!";
+  //   const headline = document.createElement("div");
+  //   headline.classList.add("headline");
+  //   headline.textContent = "Best Restoraunt!";
 
-  const catcher = document.createElement("div");
-  catcher.classList.add("catcher");
-  catcher.textContent = "Wow this is rly the bst restoraugnt!";
+  //   const catcher = document.createElement("div");
+  //   catcher.classList.add("catcher");
+  //   catcher.textContent = "Wow this is rly the bst restoraugnt!";
 
   //   content.appendChild(headline);
   //   content.appendChild(catcher);
