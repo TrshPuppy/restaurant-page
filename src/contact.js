5e221fff05124084744155a6239804ed1973e15c
Rose Pineau <rosepineau@protonmail.com>
2022-09-11 19:32:36 -0400

    Import and use Element module in contact.js.

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,12 @@
+import Element from "./elementClass";
+
 const content = document.querySelector("#content");
 
 export default function loadContact() {
-  const titleBox = createTitleBox("Contact", "This is the contact page");
+  const contactElement = new Element("div")
+    .addAttributes({ class: "contact-tab", id: "contact-tab" })
+    .addChild(new Element("h1").setInnerText("Contact Us!"))
+    .addChild(new Element("div").addAttributes({ class: "contact-div" }));
 
-  content.append(titleBox);
+  content.appendChild(contactElement.build());
 }
