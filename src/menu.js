8591553b5fb86c30c9723cd0a3f56dc385898644
Rose Pineau <rosepineau@protonmail.com>
2022-09-28 12:57:38 -0400

    Finish styling menu tab.

--- src/menu.js
+++ src/menu.js
@@ -5,7 +5,7 @@ const content = document.querySelector(".content");
 export default function loadMenu() {
   const menuElement = new Element("div")
     .addAttributes({ id: "menu-tab", class: "tab" })
-    .addChild(new Element("h1").setInnerText("Menu"))
+    .addChild(new Element("h1").setInnerText("Our Daily Menu"))
     .addChild(
       new Element("div")
         .addAttributes({ class: "container-div", id: "menu-container" })
@@ -18,20 +18,40 @@ export default function loadMenu() {
         .addChild(
           new Element("div")
             .addAttributes({ class: "div-child", id: "menu-child" })
-            .addChild(
-              new Element("h1")
-                .addAttributes({ id: "menu-title-h1" })
-                .setInnerText("Our Daily Menu")
-            )
+            .addChild(new Element("h2").setInnerText("Coffees"))
             .addChild(
               new Element("ul")
-                .addAttributes({ id: "menu-list" })
+                .addAttributes({ class: "menu-list", id: "coffee-list" })
                 .addChild(new Element("li").setInnerText("Espresso"))
                 .addChild(new Element("li").setInnerText("Americano"))
                 .addChild(new Element("li").setInnerText("Cold Brew"))
                 .addChild(new Element("li").setInnerText("Cappuccino"))
                 .addChild(new Element("li").setInnerText("Macchiato"))
                 .addChild(new Element("li").setInnerText("Latte"))
+            )
+            .addChild(new Element("h2").setInnerText("Teas"))
+            .addChild(
+              new Element("ul")
+                .addAttributes({ class: "menu-list", id: "tea-list" })
+                .addChild(new Element("li").setInnerText("Chai"))
+                .addChild(new Element("li").setInnerText("London Fog"))
+                .addChild(new Element("li").setInnerText("Black Tea"))
+                .addChild(new Element("li").setInnerText("Green Tea"))
+                .addChild(new Element("li").setInnerText("Oolong"))
+                .addChild(
+                  new Element("li").setInnerText("Chamomile with cat hair")
+                )
+            )
+            .addChild(new Element("h2").setInnerText("Treats"))
+            .addChild(
+              new Element("ul")
+                .addAttributes({ class: "menu-list", id: "treats-list" })
+                .addChild(new Element("li").setInnerText("Pumpkin Bread"))
+                .addChild(new Element("li").setInnerText("Banana Bread"))
+                .addChild(new Element("li").setInnerText("Cake Pop"))
+                .addChild(
+                  new Element("li").setInnerText("Bagle w/ cream cheese")
+                )
             )
         )
     );
